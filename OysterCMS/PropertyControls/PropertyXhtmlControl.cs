ff2da32138eec29bd132f2d6c6ec721277d0d340
Tomas Bäckström <fleptut@gmail.com>
2011-09-04 21:54:31 +0200

    Added TinyMCE resources and XHTML-editor control type

---
+++ OysterCMS/PropertyControls/PropertyXhtmlControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OysterCMS.PropertyControls
+{
+    public class PropertyXhtmlControl : PropertyControl
+    {
+        private TextBox inputBox = new TextBox();
+
+        public override void CreateChildControls(Control container)
+        {
+            inputBox.ID = PropertyName;
+            inputBox.ClientIDMode = System.Web.UI.ClientIDMode.Static;
+            inputBox.Text = Value as string;
+
+            inputBox.CssClass = "slasktratt";
+
+            container.Controls.Add(inputBox);
+        }
+
+        public override object Value
+        {
+            get { return inputBox.Text; }
+            set { inputBox.Text = value == null ? string.Empty : value.ToString(); }
+        }
+    }
+}
